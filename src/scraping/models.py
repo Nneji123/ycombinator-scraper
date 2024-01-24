73d87156dbead84f977450b45361b40b2d1e06ec
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2024-01-24 10:23:49 +0100

    Update models.py

--- src/scraping/models.py
+++ src/scraping/models.py
@@ -1,31 +1,28 @@
 from typing import List
-
 from pydantic import BaseModel
-
 
 class FounderData(BaseModel):
     founderName: str
     founderDescription: str
     founderImageUrl: str
     founderLinkedinUrl: str
-    founderEmail: str
-
-class CompanyData(BaseModel):
-    companyName: str
-    companyDescription: str
-    companyTags: List
-    companyImage: str
-    companyLinks: List
-    companyFounders: List[FounderData]
-    jobDatas: List[JobData]
+    founderEmails: List[str]
 
 class JobData(BaseModel):
     jobDescription: str
     jobUrl: str
     jobTitle: str
     jobSalaryRange: str
-    jobTags: List
+    jobTags: List[str]
 
+class CompanyData(BaseModel):
+    companyName: str
+    companyDescription: str
+    companyTags: List[str]
+    companyImage: str
+    companyLinks: List[str]
+    companyFounders: List[FounderData]
+    jobDatas: List[JobData]
 
 class ScrapedData(BaseModel):
     scraped_data: List[CompanyData]
