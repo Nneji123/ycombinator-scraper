7d067690350d9a14e68f1ec38d88d415e0fe4c7d
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2024-01-25 07:11:41 +0100

    Update config.py

--- config/config.py
+++ config/config.py
@@ -6,7 +6,10 @@ class Settings(BaseSettings):
     csv_filename: str = "output.csv"
     login_username: str
     login_password: str
+    logs_directory: str
+    headless_mode: bool = True
     CHROME_BINARY: str = "C:/Program Files/Google/Chrome/Application/chrome.exe"
-
+    CHROMEDRIVER_BINARY: str = "chromedriver.exe"
+    
     class Config:
         env_file = ".env"
