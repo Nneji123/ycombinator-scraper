cb73b8449d1f83ead1948e42befeea4f71de4785
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2024-01-25 07:52:52 +0100

    Update __init__.py

--- src/cli/__init__.py
+++ src/cli/__init__.py
@@ -1,1 +1,8 @@
+#   -------------------------------------------------------------
+#   Copyright (c) Ifeanyi Nneji. All rights reserved.
+#   Licensed under the MIT License. See LICENSE in project root for information.
+#   -------------------------------------------------------------
+"""Python Package Template"""
 
+__version__ = "0.0.1"
+__author__= "Ifeanyi Nneji"
