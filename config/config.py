8e8418d7732c68f7d3e1e97bb09969d0f5092810
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2024-01-25 12:49:39 +0100

    Update config.py

--- config/config.py
+++ config/config.py
@@ -5,7 +5,7 @@ from pathlib import Path
 class Settings(BaseSettings):
     login_username: str
     login_password: str
-    logs_directory: str = Path("../../logs")
+    logs_directory: str = Path("./logs")
     headless_mode: bool = True
     CHROME_BINARY: str = "C:/Program Files/Google/Chrome/Application/chrome.exe"
     CHROMEDRIVER_BINARY: str = "chromedriver.exe"
