2d0db66383968c6df3b1231bf6d8e58ddb27954a
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2024-01-31 15:01:01 +0100

    Update __init__.py

--- ycombinator_scraper/__init__.py
+++ ycombinator_scraper/__init__.py
@@ -2,5 +2,6 @@
 #   Copyright (c) Ifeanyi Nneji. All rights reserved.
 #   Licensed under the MIT License. See LICENSE in project root for information.
 """Ycombinator/Workatastartup.com Scraper"""
+from ycombinator_scraper.scraper import Scraper
 
 __version__ = "0.7.1"
